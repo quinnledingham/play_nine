e403bfa9d373a8168067439d112e8cf54d4f667d
Quinn Ledingham <quinnledingham@gmail.com>
2024-06-24 16:45:19 -0600

    fixed building on linux

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 mkdir -p build
-g++  -I./glad -I./stb -I./ -I./qlib -I./steam -I$VULKAN_SDK/include -DSDL -DVULKAN -DDEBUG -DSTEAM -g -DOS_LINUX qlib/sdl_application.cpp -o build/play_nine -I/usr/include/SDL2 -L$VULKAN_SDK/lib -L/home/quinn/Stuff/play_nine/steam/redistributable_bin/linux64 -lSDL2 -lvulkan -lshaderc_combined -lspirv-cross-c-shared -lsteam_api -D_REENTRANT
+g++  -I./glad -I./stb -I./ -I./qlib -I./steam -I$VULKAN_SDK/include -DSDL -DVULKAN -DDEBUG -DSTEAM -g -DOS_LINUX qlib/sdl_application.cpp -o build/play_nine -I/usr/include/SDL2 -L$VULKAN_SDK/lib -L/home/quinn/SteamSDK/redistributable_bin/linux64 -lSDL2 -lvulkan -lshaderc_combined -lspirv-cross-c-shared -lsteam_api -D_REENTRANT
